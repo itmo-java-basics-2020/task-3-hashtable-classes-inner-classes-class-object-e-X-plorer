372d6e375c926ea05441235956b1018dc0a7f3bd
e-X-plorer <arsenicpool3d@gmail.com>
2020-03-10 19:19:38 +0300

    working solution

--- src/main/java/ru/itmo/java/HashTable.java
+++ src/main/java/ru/itmo/java/HashTable.java
@@ -5,12 +5,15 @@ import java.util.Map;
 public class HashTable {
 
     private final LoopTerminatorPredicate additionPredicate = (currentIndex, table, keyToAdd) ->
-            table[currentIndex % table.length] != null && !table[currentIndex % table.length].key.equals(keyToAdd)/*&& (!table[currentIndex % table.length].deleted || !table[currentIndex % table.length].key.equals(keyToAdd))*/;
-    private final LoopTerminatorPredicate searchPredicate = (currentIndex, table, keyToFind) ->
+            table[currentIndex % table.length] != null && !table[currentIndex % table.length].key.equals(keyToAdd);
+    private final LoopTerminatorPredicate getPredicate = (currentIndex, table, keyToFind) ->
             table[currentIndex % table.length] != null && (!table[currentIndex % table.length].key.equals(keyToFind) || table[currentIndex % table.length].deleted);
+    private final LoopTerminatorPredicate removePredicate = (currentIndex, table, keyToFind) ->
+            table[currentIndex % table.length] != null && !table[currentIndex % table.length].key.equals(keyToFind);
 
     private Entry[] table;
     private int count = 0;
+    private int dirty = 0;
     private float loadFactor = 0.5f;
 
     public HashTable(int initialCapacity) {
@@ -22,16 +25,21 @@ public class HashTable {
         this.loadFactor = loadFactor;
     }
 
-    private int find(Object key, boolean add) {
-        if (add) {
-            return find(key, table, additionPredicate);
-        } else {
-            return find(key, table, searchPredicate);
+    private int find(Object key, Operation operation) {
+        switch (operation) {
+            case PUT:
+                return find(key, table, additionPredicate);
+            case GET:
+                return find(key, table, getPredicate);
+            case REMOVE:
+                return find(key, table, removePredicate);
+            default:
+                return -1;
         }
     }
 
     private int find(Object key, Entry[] table, LoopTerminatorPredicate predicate) {
-        int hc = key.hashCode(), step = 0;
+        int hc = key.hashCode() * 0x7ffffff, step = 0;
         do {
             hc += step * step;
             if (hc < 0) {
@@ -44,7 +52,7 @@ public class HashTable {
     }
 
     private boolean checkCapacityAndExpand() {
-        if (count < threshold()) {
+        if (count + dirty < threshold()) {
             return false;
         }
         Entry[] newTable = new Entry[table.length * 2];
@@ -60,10 +68,12 @@ public class HashTable {
 
     Object put(Object key, Object value) {
         checkCapacityAndExpand();
-        int index = find(key, true);
-        Object prev;
-        if (table[index] == null || table[index].deleted) {
-            prev = null;
+        int index = find(key, Operation.PUT);
+        Object prev = null;
+        if (table[index] == null) {
+            count++;
+        } else if (table[index].deleted) {
+            dirty--;
             count++;
         } else {
             prev = table[index].value;
@@ -73,7 +83,7 @@ public class HashTable {
     }
 
     Object get(Object key) {
-        int index = find(key, false);
+        int index = find(key, Operation.GET);
         if (table[index] == null) {
             return null;
         }
@@ -81,12 +91,13 @@ public class HashTable {
     }
 
     Object remove(Object key) {
-        int index = find(key, false);
-        if (table[index] == null) {
+        int index = find(key, Operation.REMOVE);
+        if (table[index] == null || table[index].deleted) {
             return null;
         }
         table[index].deleted = true;
         count--;
+        dirty++;
         return table[index].value;
     }
 
@@ -95,7 +106,7 @@ public class HashTable {
     }
 
     int threshold() {
-        return (int) (table.length * loadFactor);
+        return (int) Math.min(table.length * loadFactor, table.length * 0.8 + 0.2);
     }
 
     private static class Entry {
@@ -112,4 +123,6 @@ public class HashTable {
     private interface LoopTerminatorPredicate {
         boolean operation(int currentIndex, Entry[] table, Object key);
     }
+
+    private enum Operation {PUT, GET, REMOVE}
 }
